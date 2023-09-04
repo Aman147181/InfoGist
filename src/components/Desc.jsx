063c33467512acefe6303c8d1ab5915a888e54c7
Aman Shrestha <amanshrestha147181@gmail.com>
2023-09-04 20:51:19 +0545

    changed tagline and description

--- src/components/Desc.jsx
+++ src/components/Desc.jsx
@@ -4,12 +4,13 @@ const Desc = () => {
   return (
     <div>
       <h1 className=' text-5xl font-extrabold leading-[1.15] text-black sm:text-6xl text-center pt-24 pb-8 '>
-        Summarize Articles with <br className='max-md:hidden' />
-        <span className='bg-gradient-to-r from-blue-500 via-violet-500 to-purple-600 bg-clip-text text-transparent '>OpenAI GPT-4</span>
+      Unlock Insights with  
+        <span className='bg-gradient-to-r from-blue-500 via-violet-500 to-purple-600 bg-clip-text text-transparent '> OpenAI GPT-4</span>
+        <br className='max-md:hidden' />
+        Article Digests
       </h1>
       <h2 className='text-lg text-gray-600 sm:text-xl text-center max-w-2xl mx-auto'>
-        Simplify your reading with InfoGist, an open-source article summarizer
-        that transforms lengthy articles into clear and concise summaries
+      Streamline your reading experience with InfoGist, a cutting-edge open-source article summarization tool. Instantly convert extensive articles into easily digestible, concise summaries
       </h2>
     </div>
   );
